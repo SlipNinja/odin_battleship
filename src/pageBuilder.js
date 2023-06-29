c971bb6f6517725bac74dcd6fc1f9335bb155c8b
Pierre Lanneau <26084911SlipNinja@users.noreply.github.com>
2023-06-29 13:07:39 +0200

    Improve basic project structure

---
+++ src/pageBuilder.js
@@ -0,0 +1,17 @@
+function buildPage() {
+
+    // Creates DOM elements
+    const mainElement = document.createElement("div");
+    mainElement.id = "mainElement";
+    mainElement.innerHTML = "HELLO THERE !";
+
+    // Links DOM elements
+
+
+    document.body.appendChild(mainElement);
+
+    // Adds events
+    
+}
+
+export { buildPage };
