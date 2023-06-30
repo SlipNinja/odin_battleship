dcf4415e3b18161eccf5881cf726005697142bc0
Pierre Lanneau <26084911SlipNinja@users.noreply.github.com>
2023-06-30 15:48:39 +0200

    Add basic page structure

--- src/pageBuilder.js
+++ src/pageBuilder.js
@@ -3,15 +3,62 @@ function buildPage() {
     // Creates DOM elements
     const mainElement = document.createElement("div");
     mainElement.id = "mainElement";
-    mainElement.innerHTML = "HELLO THERE !";
+
+    const header = buildHeader();
+    const content = buildContent();
+    const footer = buildFooter();
 
     // Links DOM elements
 
-
+    mainElement.appendChild(header);
+    mainElement.appendChild(content);
+    mainElement.appendChild(footer);
     document.body.appendChild(mainElement);
 
     // Adds events
     
+}
+
+function buildHeader() {
+    const header = document.createElement("div");
+    header.id = "header";
+
+    const logo = document.createElement("div");
+    logo.id = "logo";
+    logo.innerHTML = "&#8618 LOGO HERE";
+
+    header.appendChild(logo);
+
+    return header;
+}
+
+function buildContent() {
+    const content = document.createElement("div");
+    content.id = "content";
+
+    const leftPanel = document.createElement("div");
+    leftPanel.id = "leftPanel";
+
+    const boardsPanel = document.createElement("div");
+    boardsPanel.id = "boardsPanel";
+
+    content.appendChild(leftPanel);
+    content.appendChild(boardsPanel);
+
+    return content;
+}
+
+function buildFooter() {
+    const footer = document.createElement("div");
+    footer.id = "footer";
+
+    const mainButton = document.createElement("button");
+    mainButton.id = "mainButton";
+    mainButton.innerHTML = "Start game";
+
+    footer.appendChild(mainButton);
+
+    return footer;
 }
 
 export { buildPage };
